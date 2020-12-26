fddfd0b30826fa2b264887647dd7141409f450ec
Aloisio Arsego Wohlmeister <aloisioaw@gmail.com>
2020-12-27 00:38:48 +0100

    Adjustment

--- 3-largest_prime_factor/src/ConfigLog4j.ts
+++ 3-largest_prime_factor/src/ConfigLog4j.ts
@@ -4,8 +4,8 @@ import {LoggerFactory, LoggerFactoryOptions, LFService, LogGroupRule, LogLevel} 
 // * One for any logger with a name starting with model, to log on debug
 // * The second one for anything else to log on info
 const options = new LoggerFactoryOptions()
-.addLogGroupRule(new LogGroupRule(new RegExp("model.+"), LogLevel.Debug))
-.addLogGroupRule(new LogGroupRule(new RegExp(".+"), LogLevel.Info));
+  .addLogGroupRule(new LogGroupRule(new RegExp("model.+"), LogLevel.Debug))
+  .addLogGroupRule(new LogGroupRule(new RegExp(".+"), LogLevel.Info));
  
 // Create a named loggerfactory and pass in the options and export the factory.
 // Named is since version 0.2.+ (it's recommended for future usage)
